2a6ba66427fc3b19d1e7eea45b087836126b08a2
davide-butera <davidebtr8@example.com>
2020-12-05 15:38:45 +0100

    Update day5.py

--- day5.py
+++ day5.py
@@ -4,29 +4,19 @@ input_file = sys.argv[1]
 with open(input_file, "r") as file:
     data = [line.strip() for line in file]
 
+rules = {"L": "0", "R": "1", "F": "0", "B": "1"}
 
 # BFFFBBFRRR -> 1000110111 -> 567
-def seat_to_int_classic(seat):
-    binary_seat = ""
-    for i in seat:
-        if i in {"F", "L"}:
-            binary_seat += "0"
-        else:
-            binary_seat += "1"
-    return int(binary_seat, 2)
-
-
-def seat_to_int(seat):
-    rules = {"B": "1", "F": "0", "R": "1", "L": "0"}
+def seat_to_int(seat, rules):
     binary_seat = [rules[i] for i in seat]
     return int("".join(binary_seat), 2)
 
 
 max = 0
 seat_set = {0, 1}
-print
+
 for line in data:
-    current_line = seat_to_int(line)
+    current_line = seat_to_int(line, rules)
     seat_set.add(current_line)
     if current_line > max:
         max = current_line
